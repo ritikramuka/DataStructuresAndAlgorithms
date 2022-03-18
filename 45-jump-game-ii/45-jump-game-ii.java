9a64361df26be8cb616e1a883c175a778358fd4f
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-03-19 03:48:08 +0530

    Time: 3 ms (54.65%), Space: 49.4 MB (45.38%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,28 +1,13 @@
 class Solution {
-    public int memoJump(int[] nums, int idx, int[] dp) {
-        if(idx >= nums.length - 1) {
-            return 0;
-        }
-        
-        if(dp[idx] != -1) {
-            return dp[idx];
-        }
-        
-        int ans = Integer.MAX_VALUE;
-        for(int i = 1; i <= nums[idx]; i++) {
-            int t1 = memoJump(nums, idx + i, dp);
-            int t2 = t1 == Integer.MAX_VALUE ? Integer.MAX_VALUE : t1 + 1;
-            ans = Math.min(ans, t2);
-        }
-        
-        return dp[idx] = ans;
-    }
-    
     public int jump(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        int ans = memoJump(nums, 0, dp);
-        return ans == Integer.MAX_VALUE ? 0 : ans;
+        int farthest = 0, end = 0, jumps = 0;
+        for(int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, nums[i] + i);
+            if(i == end) {
+                jumps++;
+                end = farthest;
+            }
+        }
+        return jumps;
     }
 }
