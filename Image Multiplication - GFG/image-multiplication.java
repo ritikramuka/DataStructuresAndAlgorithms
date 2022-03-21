5743ac57de5db2a8ba393fd1a4da87059a23bbec
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-03-21 23:12:43 +0530

    Added solution - LeetHub

--- Image Multiplication - GFG/image-multiplication.java
+++ Image Multiplication - GFG/image-multiplication.java
@@ -111,23 +111,24 @@ class Node{
 }
  */
  class Solution {
+    long ans = 0;
     long MOD = 1000000007;
-    long ans;
-    public void helper(Node r1, Node r2) {
+    
+    public void mul(Node r1, Node r2) {
         if(r1 == null || r2 == null) {
             return;
         }
         
         ans += (r1.data * r2.data) % MOD;
-        helper(r1.left, r2.right);
-        helper(r1.right, r2.left);
+        mul(r1.left, r2.right);
+        mul(r1.right, r2.left);
     }
     
     public long imgMultiply(Node root)
     {
         // code here
-        ans = root.data * root.data;
-        helper(root.left, root.right);
+        ans = (root.data * root.data) % MOD;
+        mul(root.left, root.right);
         return ans % MOD;
     }
 }
