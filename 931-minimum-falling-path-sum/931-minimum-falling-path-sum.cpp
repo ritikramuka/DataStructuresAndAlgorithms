eddd5d2b5a2926f21ec1d9b9a646f931f182b0b3
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-03-21 08:29:29 +0530

    Time: 15 ms (77.14%), Space: 9.9 MB (75.42%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -6,23 +6,25 @@ public:
             return matrix[0][0];
         }
         
-        vector<vector<int>> dp(n, vector<int> (n, 0));
+        vector<int> dp(n, 0);
+        vector<int> dpTemp(n, 0);
         for(int col = 0; col < n; col++) {
-            dp[n - 1][col] = matrix[n - 1][col];
+            dp[col] = matrix[n - 1][col];
         }
         
         int ans = INT_MAX;
         for(int row = n - 2; row >= 0; row--) {
             for(int col = 0; col < n; col++) {
-                int b = dp[row + 1][col];
-                int l = col - 1 >= 0 ? dp[row + 1][col - 1] : INT_MAX;
-                int r = col + 1 < n ? dp[row + 1][col + 1] : INT_MAX;
+                int b = dp[col];
+                int l = col - 1 >= 0 ? dp[col - 1] : INT_MAX;
+                int r = col + 1 < n ? dp[col + 1] : INT_MAX;
                 
-                dp[row][col] = matrix[row][col] + min({b, l, r});
+                dpTemp[col] = matrix[row][col] + min({b, l, r});
                 if(row == 0) {
-                    ans = min(ans, dp[row][col]);
+                    ans = min(ans, dpTemp[col]);
                 }
             }
+            dp = dpTemp;
         }
         
         return ans;
