e72c0507b33b3c210560fafd9ac451c65723d71e
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-03-26 15:24:14 +0530

    Time: 36 ms (88.84%), Space: 27.5 MB (92.40%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+            if(nums[mid] == target) 
+                return mid;
+            else if(nums[mid] > target) 
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return -1;
+    }
+};
