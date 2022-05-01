53c3edac355d038211a5052c40cefdd5bd951498
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-05-01 15:04:03 +0530

    Time: 0 ms (100.00%), Space: 6 MB (97.67%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,40 +1,48 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> sdash;
-        stack<char> tdash;
-        
-        for(char ch : s) {
-            if(ch == '#') {
-                if(sdash.size() > 0) {
-                    sdash.pop();
+        int i = s.size() - 1, j = t.size() - 1;
+        while(i >= 0 || j >= 0) {
+            int backSpace = 0;
+            while(i >= 0) {
+                if(s[i] == '#') {
+                    i--;
+                    backSpace++;
+                } else if(backSpace > 0) {
+                    i--;
+                    backSpace--;
+                } else {
+                    break;
                 }
-            } else {
-                sdash.push(ch);
             }
-        }
-        
-        for(char ch : t) {
-            if(ch == '#') {
-                if(tdash.size() > 0) {
-                    tdash.pop();
+            
+            backSpace = 0;
+            while(j >= 0) {
+                if(t[j] == '#') {
+                    j--;
+                    backSpace++;
+                } else if(backSpace > 0) {
+                    j--;
+                    backSpace--;
+                } else {
+                    break;
                 }
-            } else {
-                tdash.push(ch);
             }
-        }
-        
-        if(sdash.size() != tdash.size()) {
-            return false;
-        }
-        
-        while(sdash.size() != 0) {
-            if(sdash.top() != tdash.top()) {
+            
+            if(i < 0 && j < 0) {
+                return true;
+            }
+            
+            if(i < 0 || j < 0) {
                 return false;
-            } else {
-                sdash.pop();
-                tdash.pop();
             }
+            
+            if(s[i] != t[j]) {
+                return false;
+            }
+            
+            i--;
+            j--;
         }
         
         return true;
