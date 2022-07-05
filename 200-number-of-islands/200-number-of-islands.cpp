6bfffa4ed62916692d94d775e9abd49e5318b206
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-07-05 20:39:42 +0530

    Time: 40 ms (84.00%), Space: 12.3 MB (83.95%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    int dir[4][2] = {{1, 0}, {0, 1}, {0, -1}, {-1, 0}};
+    int dir[4][2] = {{-1, 0}, {0, 1}, {0, -1}, {1, 0}};
+    int n, m;
     
-    void dfs(vector<vector<char>>& grid, int sr, int sc, int er, int ec) {
-        if(sr < 0 || sr >= er || sc < 0 || sc >= ec || grid[sr][sc] != '1') {
-            return;
-        }
-        
-        grid[sr][sc] = 3;
+    void dfs(vector<vector<char>>& arr, int r, int c) {
+        arr[r][c] = '2';
         for(int d = 0; d < 4; d++) {
-            int nr = sr + dir[d][0];
-            int nc = sc + dir[d][1];
-            dfs(grid, nr, nc, er, ec);
+            int nr = r + dir[d][0];
+            int nc = c + dir[d][1];
+            if(nr >= 0 && nr < n && nc >= 0 && nc < m && arr[nr][nc] == '1') {
+                dfs(arr, nr, nc);
+            }
         }
-        
-        return;
     }
     
     int numIslands(vector<vector<char>>& grid) {
-        // 1 -> land
-        // 2 -> water
-        // 3 -> explored land
-        
-        int n = grid.size();
-        int m = grid[0].size();
-        
+        n = grid.size();
+        m = grid[0].size();
         int ans = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(grid[i][j] == '1') {
+                    dfs(grid, i, j);
                     ans++;
-                    dfs(grid, i, j, n, m);
                 }
             }
         }
-        
         return ans;
     }
 };
