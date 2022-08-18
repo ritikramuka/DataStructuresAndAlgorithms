2d82fd1cedfea6e6624e572e012104a4224841d8
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-08-19 00:58:26 +0530

    Time: 27 ms (41.20%), Space: 14.7 MB (24.50%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,59 @@
+class Solution
+{
+    public:
+        /*
+        bool rec(string s, int itr, unordered_set<string> &uset)
+        {
+            if(itr == s.size())
+            {
+                return true;
+            }
+            
+            bool ans = false;
+            for (int i = itr + 1; i <= s.size(); i++)
+            {
+                string sstr = s.substr(itr, i - itr);
+                if (uset.find(sstr) != uset.end())
+                {
+                    ans |= rec(s, i, uset);
+                }
+            }
+            
+            return ans;
+        }
+        **/
+        
+        int dp[301];
+        int memo(string s, int itr, unordered_set<string> &uset)
+        {
+            if(itr == s.size())
+            {
+                return dp[itr] = 1;
+            }
+            
+            if(dp[itr] != -1) 
+            {
+                return dp[itr];
+            }
+            
+            int ans = 0;
+            for (int i = itr + 1; i <= s.size(); i++)
+            {
+                string sstr = s.substr(itr, i - itr);
+                if (uset.find(sstr) != uset.end())
+                {
+                    ans |= memo(s, i, uset); 
+                }
+            }
+            
+            return dp[itr] = ans;
+        }
+    
+        bool wordBreak(string s, vector<string> &wordDict)
+        {
+            memset(dp, -1, sizeof(dp));
+            unordered_set<string> uset(wordDict.begin(), wordDict.end());
+            int ans = memo(s, 0, uset);
+            return ans == 1;
+        }
+}; 
