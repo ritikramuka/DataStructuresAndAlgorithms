0661de63e943c330ee324a61b29c9dab713ed092
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-08-19 01:02:44 +0530

    Time: 24 ms (45.56%), Space: 14.7 MB (20.88%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -8,7 +8,7 @@ class Solution
             {
                 return true;
             }
-            
+
             bool ans = false;
             for (int i = itr + 1; i <= s.size(); i++)
             {
@@ -18,37 +18,37 @@ class Solution
                     ans |= rec(s, i, uset);
                 }
             }
-            
+
             return ans;
         }
         **/
-        
+
         int dp[301];
         int memo(string s, int itr, unordered_set<string> &uset)
         {
-            if(itr == s.size())
+            if (itr == s.size())
             {
                 return dp[itr] = 1;
             }
-            
-            if(dp[itr] != -1) 
+
+            if (dp[itr] != -1)
             {
                 return dp[itr];
             }
-            
+
             int ans = 0;
-            for (int i = itr + 1; i <= s.size(); i++)
+            for (int i = itr; i < s.size(); i++)
             {
-                string sstr = s.substr(itr, i - itr);
+                string sstr = s.substr(itr, i - itr + 1);
                 if (uset.find(sstr) != uset.end())
                 {
-                    ans |= memo(s, i, uset); 
+                    ans |= memo(s, i + 1, uset);
                 }
             }
-            
+
             return dp[itr] = ans;
         }
-    
+
         bool wordBreak(string s, vector<string> &wordDict)
         {
             memset(dp, -1, sizeof(dp));
@@ -56,4 +56,4 @@ class Solution
             int ans = memo(s, 0, uset);
             return ans == 1;
         }
-}; 
+};
