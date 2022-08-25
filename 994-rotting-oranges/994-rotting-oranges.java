5f89246d6fcfb7a9850ed0bee13c4f5d831ab751
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-08-25 22:44:14 +0530

    Time: 4 ms (46.26%), Space: 42.7 MB (72.13%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,11 +1,20 @@
 class Solution {
+    public class Pair {
+        int row;
+        int col;
+        
+        Pair() {};
+        Pair(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+    }
+    
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         
-        Queue<Pair> que = new ArrayDeque<>();
-        // all intitally rotten oranges
+        Queue<Pair> que = new LinkedList<>();
         for(int r = 0; r < n; r++) {
             for(int c = 0; c < m; c++) {
                 if(grid[r][c] == 2) {
@@ -13,42 +22,50 @@ class Solution {
                 }
             }
         }
+        
         int time = 0;
-        while(que.size() != 0) {
+        while(!que.isEmpty()) {
             int size = que.size();
             while(size-->0) {
-                Pair rm = que.remove();
-                for(int i = 0; i < 4; i++) {
-                    int nr = rm.row + dir[i][0];
-                    int nc = rm.col + dir[i][1];
-                    
-                    if(nr >= 0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] == 1) {
-                        grid[nr][nc] = 2;
-                        que.add(new Pair(nr, nc));
-                    }
+                Pair rp = que.poll();
+                int cr = rp.row;
+                int cc = rp.col;
+                
+                // right
+                if(cc + 1 < m && grid[cr][cc + 1] == 1) {
+                    grid[cr][cc + 1] = 2;
+                    que.add(new Pair(cr, cc + 1));
+                }
+                
+                // left
+                if(cc - 1 >= 0 && grid[cr][cc - 1] == 1) {
+                    grid[cr][cc - 1] = 2;
+                    que.add(new Pair(cr, cc - 1));
+                }
+                
+                // top
+                if(cr - 1 >= 0 && grid[cr - 1][cc] == 1) {
+                    grid[cr - 1][cc] = 2;
+                    que.add(new Pair(cr - 1, cc));
+                }
+                
+                // bottom
+                if(cr + 1 < n && grid[cr + 1][cc] == 1) {
+                    grid[cr + 1][cc] = 2;
+                    que.add(new Pair(cr + 1, cc));
                 }
             }
             time++;
         }
         
-        // cheack for fresh orange 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 1) {
+        for(int r = 0; r < n; r++) {
+            for(int c = 0; c < m; c++) {
+                if(grid[r][c] == 1) {
                     return -1;
                 }
             }
         }
         
         return time - 1 < 0 ? 0 : time - 1;
-    }
-    
-    class Pair {
-        int row;
-        int col;
-        Pair(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
     }
 }
