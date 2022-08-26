0596ca57857a2c3e21a8bef71e2d9c6aba68fbde
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-08-27 05:13:06 +0530

    Time: 56 ms (98.93%), Space: 21.8 MB (56.17%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int dir[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
     int n, m;
-    int cnt = 0;
+    
     void dfs_closeIsland(vector<vector<int>>& grid, int r, int c) {
         grid[r][c] = 0;
-        cnt++;
+        
         for(int d = 0; d < 4; d++) {
             int nr = r + dir[d][0];
             int nc = c + dir[d][1];
-            if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1) {
+            if(nr >= 0 and nr < n and nc >= 0 and nc < m and grid[nr][nc] == 1) {
                 dfs_closeIsland(grid, nr, nc);
             }
         }
@@ -18,6 +18,16 @@ public:
     int numEnclaves(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
+        
+        for(int i = 0; i < n; i++) {
+            if(grid[i][0] == 1) dfs_closeIsland(grid, i, 0);
+            if(grid[i][m - 1] == 1) dfs_closeIsland(grid, i, m - 1);
+        }
+        for(int j = 0; j < m; j++) {
+            if(grid[0][j] == 1) dfs_closeIsland(grid, 0, j);
+            if(grid[n - 1][j] == 1) dfs_closeIsland(grid, n - 1, j);
+        }
+        
         int ans = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
@@ -25,22 +35,6 @@ public:
             }
         }
         
-        for(int i = 0; i < n; i++) {
-            if(grid[i][0] == 1) {
-                dfs_closeIsland(grid, i, 0);
-            }
-            if(grid[i][m - 1] == 1) {
-                dfs_closeIsland(grid, i, m - 1);
-            }
-        }
-        for(int j = 0; j < m; j++) {
-            if(grid[0][j] == 1) {
-                dfs_closeIsland(grid, 0, j);
-            }
-            if(grid[n - 1][j] == 1) {
-                dfs_closeIsland(grid, n - 1, j);
-            }
-        }
-        return ans - cnt;
+        return ans;
     }
 };
