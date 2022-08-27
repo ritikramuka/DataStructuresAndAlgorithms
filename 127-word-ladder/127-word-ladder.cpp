9892e20cc8ad8c195a9859c52470ed0be41c7084
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-08-27 05:34:12 +0530

    Time: 352 ms (52.84%), Space: 29 MB (18.97%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,57 +1,45 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        int n = wordList.size();
-        unordered_map<string, int> umap;
-        bool hasStart = false, hasEnd = false;
-        for(int i = 0; i < n; i++) {
-            string word = wordList[i];
-            if(word == beginWord) hasStart = true;
-            umap[word] = i;
-        }
-        
-        if(hasStart == false) {
-            wordList.push_back(beginWord);
-            umap[beginWord] = n;
-            n++;
-        }
-        
-        vector<vector<int>> gp(n);
-        for(int i = 0; i < n; i++) {
-            string word = wordList[i];
-            for(int j = 0; j < word.size(); j++) {
-                for(int k = 0; k < 26; k++) {
-                    word[j] = (k + 'a');
-                    if((umap.find(word) != umap.end())) {
-                        gp[umap[word]].push_back(umap[wordList[i]]);
-                        gp[umap[wordList[i]]].push_back(umap[word]);
+        wordList.push_back(beginWord);
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        unordered_map<string, unordered_set<string>> gp;
+        for(string word : wordList) {
+            for(int i = 0; i < word.size(); i++) {
+                string w = word;
+                char ch = word[i];
+                for(int j = 0; j < 26; j++) {
+                    word[i] = (char)(j + 'a');
+                    if(word[i] != ch and dict.find(word) != dict.end()) {
+                        gp[w].insert(word);
                     }
                 }
-                word = wordList[i];
+                word = w;
             }
         }
         
-        int len = 1;
-        int si = umap[beginWord];
-        queue<int> que;
-        que.push(si);
-        vector<int> vis(n, false);
+        queue<string> que;
+        que.push(beginWord);
+        dict.erase(beginWord);
+        int trans = 1;
         while(que.size() != 0) {
             int size = que.size();
-            while(size-->0) {
-                int ridx = que.front();
+            while(size--> 0) {
+                string rs = que.front();
                 que.pop();
-                if(vis[ridx] == false) {
-                    vis[ridx] = true;
-                    for(int i : gp[ridx]) {
-                        if(wordList[i] == endWord) return len + 1;
-                        if(vis[i] == false) {
-                            que.push(i);
-                        }
+                
+                for(string str : gp[rs]) {
+                    if(str == endWord) {
+                        return trans + 1;
+                    }
+                    
+                    if(dict.find(str) != dict.end()) {
+                        dict.erase(str);
+                        que.push(str);
                     }
                 }
             }
-            len++;
+            trans++;
         }
         
         return 0;
