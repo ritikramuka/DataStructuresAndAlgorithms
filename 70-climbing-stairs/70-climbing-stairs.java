469fa7a8c6703f18ce591048a48092c5844504aa
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-08-31 16:33:08 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (73.10%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,19 +1,16 @@
 class Solution {
-    public int rec(int n, int[] memory) {
-        if(n == 0) return memory[n] = 1;
-        if(n == 1) return memory[n] = 1;
-        
-        if(memory[n] != 0) return memory[n];
-        
-        int oneStep = rec(n - 1, memory);
-        int twoStep = rec(n - 2, memory);
-        
-        return memory[n] = (oneStep + twoStep);
-    }
-    
     public int climbStairs(int n) {
-        int[] memory = new int[n + 1];
-        int ans = rec(n, memory);
-        return ans;
+        if(n == 1 || n == 2) {
+            return n;
+        }
+        
+        int prev1 = 2, prev2 = 1;
+        for(int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        
+        return prev1;
     }
 }
