2d2b2825f47035e7473ad9fbb01362b37d11ba5a
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-09-15 18:59:03 +0530

    Time: 3 ms (46.96%), Space: 41.9 MB (65.26%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,8 +1,13 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
         sentence = sentence.toLowerCase();
+        boolean[] alpha = new boolean[26];
+        for(int i = 0; i < sentence.length(); i++) {
+            char ch = sentence.charAt(i);
+            alpha[ch - 'a'] = true;
+        }
         for(int i = 0; i < 26; i++) {
-            if(sentence.indexOf('a' + i) == -1) {
+            if(alpha[i] == false) {
                 return false;
             }
         }
