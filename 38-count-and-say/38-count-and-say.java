6393056c5c64097e188c3df3934fca343cbd09d3
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-09-27 21:52:51 +0530

    Time: 63 ms (8.78%), Space: 60 MB (28.78%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String countAndSay(int n) {
+
+                if (n == 1) 
+                {
+                        System.out.println("1");
+                }
+
+                String ans = "1";
+                for (int i = 2; i <= n; i++)
+                {
+                        String nextAns = "";
+                        int j = 1;
+                        char ch = ans.charAt(0);
+                        int cnt = 1;
+                        while (j < ans.length())
+                        {
+                                if (ch == ans.charAt(j))
+                                {
+                                        cnt++;
+                                }
+                                else
+                                {
+                                        nextAns += cnt + "" + ch;
+                                        ch = ans.charAt(j);
+                                        cnt = 1;
+                                }
+                                j++;
+                        }
+                        nextAns += cnt + "" + ch;
+                        ans = nextAns;
+                }
+        return ans;
+    }
+}
