86bac676d250d75a07199c39a4d0c3bebe611e00
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-10-18 22:38:59 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
@@ -7,20 +7,24 @@ class Solution {
 	public:
 		int LongestRepeatingSubsequence(string str){
 		    // Code here
-		    int n = str.length();
+		    string text1 = str;
+		    string text2 = str;
 		    
-		    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-		    for (int i = n - 1; i >= 0; i--) {
-		        for (int j = n - 1; j >= 0; j--) {
-		            if (i != j and str[i] == str[j]) {
-		                dp[i][j] = dp[i + 1][j + 1] + 1;
-		            } else {
-		                dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
-		            }
-		        }
-		    }
-		    
-		    return dp[0][0];
+		    int n = text1.size();
+            int m = text2.size();
+            
+            vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
+            for (int i = n - 1; i >= 0; i--) {
+                for (int j = m - 1; j >= 0; j--) {
+                    if (text1[i] == text2[j] and i != j) {
+                        dp[i][j] = dp[i + 1][j + 1] + 1;
+                    } else {
+                        dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
+                    }
+                }
+            }
+            
+            return dp[0][0];
 		}
 
 };
