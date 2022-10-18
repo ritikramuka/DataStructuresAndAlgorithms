cd0bfa00fe3826d2d323864b488ba3fc7c74386a
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-10-18 13:09:26 +0530

    Time: 5462 ms (43.92%), Space: 14.2 MB (81.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,19 +1,13 @@
-class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-        int maxLen = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
-            maxLen = Math.max(maxLen, dp[i]);
-        }
-        
-        return maxLen;
-    }
-}
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+		
+        dp = [1]*n
+        maxLen = 1
+        for i in range(n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)       
+            maxLen = max(maxLen, dp[i])
+		
+        return maxLen
