47414e6723c270d8a99eb4fadf240ef89de429a5
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-10-18 22:04:39 +0530

    Time: 32 ms (47.82%), Space: 9.1 MB (74.82%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -12,16 +12,16 @@ public:
         for (int i = 0; i < n; i++) {
             dp[i] = cuboids[i][2];
         }
-        int maxHeight = cuboids[0][2];
-        for (int i = 1; i < n; i++) {
+        int maxHeightOfStack = 0;
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (cuboids[j][0] <= cuboids[i][0] and cuboids[j][1] <= cuboids[i][1] and cuboids[j][2] <= cuboids[i][2]) {
+                if (cuboids[i][0] >= cuboids[j][0] and cuboids[i][1] >= cuboids[j][1] and cuboids[i][2] >= cuboids[j][2]) {
                     dp[i] = max(dp[i], dp[j] + cuboids[i][2]);
                 }
             }
-            maxHeight = max(maxHeight, dp[i]);
+            maxHeightOfStack = max(maxHeightOfStack, dp[i]);
         }
         
-        return maxHeight;
+        return maxHeightOfStack;
     }
 };
