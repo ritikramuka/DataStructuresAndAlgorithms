822f2c62495a698d980b8cfac47b88e529664618
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-10-18 21:50:02 +0530

    Time: 962 ms (10.55%), Space: 77.6 MB (37.17%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         int n = envelopes.size();
+        
         sort(envelopes.begin(), envelopes.end(), [](auto& a, auto& b) {
             if (a[0] == b[0]) {
-                return a[1] > b[1];  
-            }
+                return a[1] > b[1];
+            } 
             return a[0] < b[0];
         });
         
         vector<int> temp;
-        int russianDollsNum = 0;
         for (int i = 0; i < n; i++) {
             int idx = lower_bound(temp.begin(), temp.end(), envelopes[i][1]) - temp.begin();
             if (idx == temp.size()) {
-                russianDollsNum++;
                 temp.push_back(envelopes[i][1]);
             } else {
                 temp[idx] = envelopes[i][1];
             }
         }
         
-        return russianDollsNum;
+        return temp.size();
     }
 };
