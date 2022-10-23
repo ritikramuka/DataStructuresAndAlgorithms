ff1a4482f79a0598b95466d14ca5d6683c7b00fb
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-10-24 03:30:51 +0530

    Time: 83 ms (47.31%), Space: 21.5 MB (66.74%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -4,18 +4,18 @@ public:
         int n = nums.size();
         int xor1 = 0;
         for (int i = 1; i <= n; i++) {
-            xor1 ^= i;
+            xor1 = xor1 xor i;
         }
         int occTwice = 1;
         for (int i = 0; i < n; i++) {
             int num = abs(nums[i]);
-            xor1 ^= num;
+            xor1 = xor1 xor num;
             if (nums[num - 1] > 0) {
                 nums[num - 1] = -nums[num - 1];
             } else {
                 occTwice = num;
             }
         }
-        return {occTwice, xor1 ^ occTwice};
+        return {occTwice, xor1 xor occTwice};
     }
 };
