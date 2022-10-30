677a34f66c3b3d3c24216b31afcd68eebad42226
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-10-31 04:01:46 +0530

    Time: 1448 ms (7.91%), Space: 144.7 MB (98.56%) - LeetHub

---
+++ 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.cpp
@@ -0,0 +1,75 @@
+class Location {
+private:
+    string name;
+    int score;
+
+public:
+    Location (string name, int score) {
+        this->name = name;
+        this->score = score;
+    }
+    
+    string getName () {
+        return this->name;
+    }
+    
+    int getScore () {
+        return this->score;
+    }
+};
+
+class MaxHeapComp {
+public:
+    bool operator() (Location* a, Location* b) {
+        if (a->getScore() == b->getScore()) {
+            return a->getName() > b->getName();
+        }  
+        return a->getScore() < b->getScore();
+    };
+};
+
+class MinHeapComp {
+public:
+    bool operator() (Location* a, Location* b) {
+        if (a->getScore() == b->getScore()) {
+            return a->getName() < b->getName();
+        }  
+        return a->getScore() > b->getScore();
+    };
+};
+
+
+class SORTracker {
+private:
+    priority_queue<Location*, vector<Location*>, MaxHeapComp> maxHeap;
+    priority_queue<Location*, vector<Location*>, MinHeapComp> minHeap;
+    int getCalls = 0;
+    
+public:
+    SORTracker() {
+        
+    }
+    
+    void add(string name, int score) {
+        minHeap.push(new Location(name, score));
+        if (minHeap.size() > getCalls) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    
+    string get() {
+        getCalls++;
+        Location* ans = maxHeap.top();
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+        return ans->getName();
+    }
+};
+
+/**
+ * Your SORTracker object will be instantiated and called as such:
+ * SORTracker* obj = new SORTracker();
+ * obj->add(name,score);
+ * string param_2 = obj->get();
+ */
