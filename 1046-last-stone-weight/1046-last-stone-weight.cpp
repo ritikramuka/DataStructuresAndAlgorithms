241cb3bcaa059a1b714c2b43098361f486f5b071
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-10-30 17:52:31 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (78.21%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int> pq(stones.begin(), stones.end());
-        while(pq.size() > 1) {
-            int t1 = pq.top();
-            pq.pop();
-            int t2 = pq.top();
-            pq.pop();
-            if(t1 != t2) {
-                pq.push(t1 - t2);
+        priority_queue<int> maxHeap (stones.begin(), stones.end());
+        while (maxHeap.size() >= 2) {
+            int stone1 = maxHeap.top();
+            maxHeap.pop();
+            int stone2 = maxHeap.top();
+            maxHeap.pop();
+            
+            int remain = stone1 - stone2;
+            if (remain != 0) {
+                maxHeap.push(remain);
             }
         }
-        
-        return pq.size() == 0 ? 0 : pq.top();
+        return maxHeap.size() == 0 ? 0 : maxHeap.top();
     }
 };
