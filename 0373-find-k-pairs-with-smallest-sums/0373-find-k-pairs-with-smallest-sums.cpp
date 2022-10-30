d4a39aebe591a6e010a3e39be27f1f499260d8ec
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-10-30 20:39:03 +0530

    Time: 440 ms (78.56%), Space: 121.2 MB (22.08%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -4,18 +4,18 @@ public:
         auto comp = [](auto& a, auto& b) {
             return a[0] + a[1] > b[0] + b[1];
         };
-        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> maxHeap (comp);
+        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> minHeap (comp);
         for (int i = 0; i < nums1.size() && i < k; i++) {
-            maxHeap.push({nums1[i], nums2[0], 0});
+            minHeap.push({nums1[i], nums2[0], 0});
         }
         
         vector<vector<int>> res;
-        while(k-- > 0 && maxHeap.size() != 0){
-            vector<int> cur = maxHeap.top();
-            maxHeap.pop();
+        while(k-- > 0 && minHeap.size() != 0){
+            vector<int> cur = minHeap.top();
+            minHeap.pop();
             res.push_back({cur[0], cur[1]});
             if(cur[2] == nums2.size() - 1) continue;
-            maxHeap.push({cur[0], nums2[cur[2] + 1], cur[2] + 1});
+            minHeap.push({cur[0], nums2[cur[2] + 1], cur[2] + 1});
         }
         return res;
     }
