6a458fbed47348a96a7b7b66c2fc18cdb7582bfb
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-10-30 22:37:10 +0530

    Time: 887 ms (13.99%), Space: 121.2 MB (22.08%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
+    
+    // {sum, nums1_idx, nums2_idx}
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         auto comp = [](auto& a, auto& b) {
-            return a[0] + a[1] > b[0] + b[1];
+            return a[0] > b[0];    
         };
-        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> minHeap (comp);
-        for (int i = 0; i < nums1.size() && i < k; i++) {
-            minHeap.push({nums1[i], nums2[0], 0});
+        
+        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> minHeap(comp);
+        for (int i = 0; i < k && i < nums1.size(); i++) {
+            minHeap.push({nums1[i] + nums2[0], i, 0});
         }
         
-        vector<vector<int>> res;
-        while(k-- > 0 && minHeap.size() != 0){
-            vector<int> cur = minHeap.top();
+        vector<vector<int>> ans;
+        while (k-- > 0 && minHeap.size() != 0) {
+            vector<int> temp = minHeap.top();
             minHeap.pop();
-            res.push_back({cur[0], cur[1]});
-            if(cur[2] == nums2.size() - 1) continue;
-            minHeap.push({cur[0], nums2[cur[2] + 1], cur[2] + 1});
+            ans.push_back({nums1[temp[1]], nums2[temp[2]]});
+            if (temp[2] + 1 < nums2.size()) {
+                minHeap.push({nums1[temp[1]] + nums2[temp[2] + 1], temp[1], temp[2] + 1});
+            }
         }
-        return res;
+        
+        return ans;
     }
 };
