7dcbab5d0e02ff0430ea4c3cad2cceb592bef1f9
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-10-30 21:17:25 +0530

    Time: 479 ms (55.03%), Space: 69 MB (55.52%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    float dist(int x, int y) {
-        return sqrt(x*x + y*y);
+    // Euclidean distance
+    float dist (int x, int y) {
+        // distance from origin
+        return sqrt (x * x + y * y);
     }
     
+    // pq -> {dist, {x, y}}
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        int n = points.size();
-        priority_queue<pair<float, pair<int, int>>> pq;
-        for (int i = 0; i < k; i++) {
-            float d = dist(points[i][0], points[i][1]);
-            pq.push({d, {points[i][0], points[i][1]}});
+        priority_queue<pair<float, pair<int, int>>> maxHeap;
+        for (int i = 0; i < k && i < points.size(); i++) {
+            vector<int> point = points[i];
+            float myDist = dist(point[0], point[1]);
+            maxHeap.push({myDist, {point[0], point[1]}});
         }
         
-        for (int i = k; i < n; i++) {
-            float d = pq.top().first;
-            float myDist = dist(points[i][0], points[i][1]);
-            if (myDist < d) {
-                pq.pop();
-                pq.push({myDist, {points[i][0], points[i][1]}});
+        for (int i = k; i < points.size(); i++) {
+            vector<int> point = points[i];
+            float myDist = dist(point[0], point[1]);
+            if (myDist < maxHeap.top().first) {
+                maxHeap.pop();
+                maxHeap.push({myDist, {point[0], point[1]}});
             }
         }
         
         vector<vector<int>> ans;
-        while (pq.size() != 0) {
-            ans.push_back({pq.top().second.first, pq.top().second.second});
-            pq.pop();
+        while (maxHeap.size() != 0) {
+            ans.push_back({maxHeap.top().second.first, maxHeap.top().second.second});
+            maxHeap.pop();
         }
         
         return ans;
