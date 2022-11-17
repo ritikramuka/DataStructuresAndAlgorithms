2a30cd3eb83fcda678aa806d31b82cff6c14a31d
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2022-11-17 21:48:59 +0530

    Added solution - LeetHub

--- Count Alphabets - GFG/count-alphabets.java
+++ Count Alphabets - GFG/count-alphabets.java
@@ -30,17 +30,15 @@ class GFG{
 class Solution{
     static int Count(String str)
     {
-        // code here
         int cnt = 0;
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            // if (Character.isAlphabetic(ch) == true)
             if (ch >= 'a' && ch <= 'z') {
                 cnt++;
-            } 
+            }
             else if (ch >= 'A' && ch <= 'Z') {
                 cnt++;
-            }        
+            }
         }
         
         return cnt;
