2d5be1ef76566babc93f99799a7b5d5a8fe05e84
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2023-01-23 21:01:26 +0530

    Time: 1 ms (80.94%), Space: 41.4 MB (41.40%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,34 +15,42 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> rv = new ArrayList<>();
+       List<Integer> rv = new ArrayList<>();
         
+//         if no root, hence return empty List
         if (root == null) {
             return rv;
         }
         
         Queue<TreeNode> que = new ArrayDeque<>();
-        que.add (root);
+//         step 1: add root
+        que.add(root);
         
+//         step 2: do your work till que size is greater than zero
         while (que.size() > 0) {
+//             step 3: get curr size of the que
             int size = que.size();
+            
+//             step 4: remove size amt of fronts from the que and add there chilren, these size amt of people will lie on same level
             while (size > 0) {
                 TreeNode rnode = que.remove();
                 
+//                 size == 1, means removing last node of the currLevel
                 if (size == 1) {
-                    rv.add (rnode.val);
+                    rv.add(rnode.val);
                 }
-                
+
+//              step 5: see if children are there, add them   
                 if (rnode.left != null) {
-                    que.add (rnode.left);
+                    que.add(rnode.left);
                 }
-                
                 if (rnode.right != null) {
-                    que.add (rnode.right);
+                    que.add(rnode.right);
                 }
                 
                 size--;
             }
+            
         }
         
         return rv;
