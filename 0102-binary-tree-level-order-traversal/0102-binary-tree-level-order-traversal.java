097faf35971e7699a8e46e3ddafb8d292e58c879
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2023-01-23 20:59:08 +0530

    Time: 2 ms (28.92%), Space: 43.8 MB (12.67%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -17,31 +17,38 @@ class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> lo = new ArrayList<>();
         
+//         if no root, hence return empty List
         if (root == null) {
             return lo;
         }
         
         Queue<TreeNode> que = new ArrayDeque<>();
-        que.add (root);
+//         step 1: add root
+        que.add(root);
         
+//         step 2: do your work till que size is greater than zero
         while (que.size() > 0) {
-            List<Integer> level = new ArrayList<>();
-            
+//             step 3: get curr size of the que
             int size = que.size();
-            while (size-- > 0) {
+            List<Integer> currLevel = new ArrayList<>();
+            
+//             step 4: remove size amt of fronts from the que and add there chilren, these size amt of people will lie on same level
+            while (size > 0) {
                 TreeNode rnode = que.remove();
-                level.add (rnode.val);
-                
+                currLevel.add(rnode.val);
+
+//              step 5: see if children are there, add them   
                 if (rnode.left != null) {
-                    que.add (rnode.left);
+                    que.add(rnode.left);
+                }
+                if (rnode.right != null) {
+                    que.add(rnode.right);
                 }
                 
-                if (rnode.right != null) {
-                    que.add (rnode.right);
-                }
+                size--;
             }
             
-            lo.add (level);
+            lo.add(currLevel);
         }
         
         return lo;
