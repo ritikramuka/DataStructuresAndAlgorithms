99727a9f5ec1e10adf237f16e0eafab708354982
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2023-01-23 21:16:06 +0530

    Time: 2 ms (27.23%), Space: 41.8 MB (43.36%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -17,32 +17,46 @@ class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> zigzag = new ArrayList<>();
         
+//         if no root, hence return empty List
         if (root == null) {
             return zigzag;
         }
         
         Queue<TreeNode> que = new ArrayDeque<>();
+//         step 1: add root
         que.add(root);
-        
         int level = 0;
         
+//         step 2: do your work till que size is greater than zero
         while (que.size() > 0) {
+//             step 3: get curr size of the que
             int size = que.size();
             List<Integer> currLevel = new ArrayList<>();
-            while (size-->0) {
+            
+//             step 4: remove size amt of fronts from the que and add there chilren, these size amt of people will lie on same level
+            while (size > 0) {
                 TreeNode rnode = que.remove();
                 currLevel.add(rnode.val);
+
+//              step 5: see if children are there, add them   
                 if (rnode.left != null) {
                     que.add(rnode.left);
-                } 
+                }
                 if (rnode.right != null) {
                     que.add(rnode.right);
                 }
+                
+                size--;
             }
-            if (level % 2 == 1) {
+            
+            if(level % 2 == 0) {
+                zigzag.add(currLevel);
+            } else {
                 Collections.reverse(currLevel);
+                zigzag.add(currLevel);
             }
-            zigzag.add(currLevel);
+            
+            
             level++;
         }
         
