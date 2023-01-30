e4c3880e2cb8d2e75d2102859c5e37e1b3c244bc
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2023-01-30 23:03:04 +0530

    Added solution - LeetHub

--- Boundary Traversal of binary tree - GFG/boundary-traversal-of-binary-tree.java
+++ Boundary Traversal of binary tree - GFG/boundary-traversal-of-binary-tree.java
@@ -109,81 +109,105 @@ class GFG
 
 class Solution
 {
-    void leftBoundry(Node node, ArrayList<Integer> lb) {
-        if (node == null) {
+    void getLeftBoundry (Node root, ArrayList<Integer> treeBoundry) 
+    {
+        if (root == null) 
+        {
             return;
+        } 
+        
+        // leaf node
+        if (root.left == null && root.right == null) 
+        {
+            // do nothing 
+            return;
+        } 
+        else 
+        {
+            treeBoundry.add (root.data);
         }
         
-        if (node.left == null && node.right == null) {
-            
-        } else {
-            lb.add(node.data);   
+        if (root.left != null) 
+        {
+            getLeftBoundry (root.left, treeBoundry);
+        } 
+        else 
+        {
+            getLeftBoundry (root.right, treeBoundry);
         }
-        
-        if (node.left != null) {
-            leftBoundry(node.left, lb);
-        } else {
-            leftBoundry(node.right, lb);
-        }
-        
-        return;
     }
     
-    void leafNodes(Node node, ArrayList<Integer> ln) {
-        if (node == null) {
+    void getBottomBoundry (Node root, ArrayList<Integer> treeBoundry) 
+    {
+        if (root == null) 
+        {
             return;
-        }
+        } 
         
-        if (node.left == null && node.right == null) {
-            ln.add(node.data);
-        }
+        // leaf node
+        if (root.left == null && root.right == null) 
+        {
+            treeBoundry.add (root.data);
+        } 
         
-        leafNodes(node.left, ln);
-        leafNodes(node.right, ln);
-        
-        return;
+        getBottomBoundry (root.left, treeBoundry);
+        getBottomBoundry (root.right, treeBoundry);
     }
     
-    void rightBoundry(Node node, ArrayList<Integer> rb) {
-        if (node == null) {
+    void getRightBoundry (Node root, ArrayList<Integer> treeBoundry) 
+    {
+        if (root == null) 
+        {
             return;
+        } 
+        
+        if (root.right != null) 
+        {
+            getRightBoundry (root.right, treeBoundry);
+        } 
+        else 
+        {
+            getRightBoundry (root.left, treeBoundry);
         }
         
-        if (node.right != null) {
-            rightBoundry(node.right, rb);
-        } else {
-            rightBoundry(node.left, rb);
+        // leaf node
+        if (root.left == null && root.right == null) 
+        {
+            // do nothing 
+            return;
+        } 
+        else 
+        {
+            treeBoundry.add (root.data);
         }
-        
-        if (node.left == null && node.right == null) {
-            
-        } else {
-            rb.add(node.data);   
-        }
-        
-        return;
     }
     
-	ArrayList <Integer> boundary(Node node)
+	ArrayList <Integer> boundary(Node root)
 	{
-	    ArrayList<Integer> treeBoundry = new ArrayList<>();
+	   ArrayList<Integer> treeBoundry = new ArrayList<>();
+	   
+	   if (root == null) 
+	   {
+	       return treeBoundry;
+	   }
 	    
-	    if (node == null) {
-	        return treeBoundry;
-	    }
-	    
-	    treeBoundry.add(node.data);
-	    
-	    if (node.left == null && node.right == null) {
-	        return treeBoundry;
-	    }
-	    
-	    leftBoundry(node.left, treeBoundry);
-	    
-	    leafNodes(node, treeBoundry);
-	    
-	    rightBoundry(node.right, treeBoundry);
-	    
-	    return treeBoundry;
+	   // add root as top boundry
+	   treeBoundry.add (root.data);
+	   
+	   if (root.left == null && root.right == null) 
+	   {
+	       return treeBoundry;
+	   }
+	   
+	   // add left boundry
+	   getLeftBoundry (root.left, treeBoundry);
+	   
+	   // add bottom boundry
+	   getBottomBoundry (root, treeBoundry);
+	   
+	   // add right boundry
+	   getRightBoundry (root.right, treeBoundry);
+	   
+	   return treeBoundry;
 	}
 }
