4cc5280c15101db2f2ec116d9c8871f25854c3d6
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2023-01-30 22:37:59 +0530

    Time: 2 ms (87.48%), Space: 41.7 MB (81.22%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,10 +1,9 @@
 class Solution {
-    public class Pair {
+    class Pair {
         int row;
         int col;
         
-        Pair() {};
-        Pair(int row, int col) {
+        Pair (int row, int col) {
             this.row = row;
             this.col = col;
         }
@@ -14,58 +13,78 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         
-        Queue<Pair> que = new LinkedList<>();
-        for(int r = 0; r < n; r++) {
-            for(int c = 0; c < m; c++) {
-                if(grid[r][c] == 2) {
-                    que.add(new Pair(r, c));
+        // store all the src, i.e rotten oranges places
+        Queue<Pair> que = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    // found a rotten orange, hence this is one of the src
+                    que.add(new Pair (i, j));
                 }
             }
         }
         
-        int time = -1;
-        while(!que.isEmpty()) {
+        // BFS
+        int level = 0;
+        while (que.size() > 0) {
             int size = que.size();
-            while(size-->0) {
-                Pair rp = que.poll();
-                int cr = rp.row;
-                int cc = rp.col;
+            while (size > 0) {
+                Pair rpair = que.remove();
+                int cr = rpair.row;
+                int cc = rpair.col;
                 
-                // right
-                if(cc + 1 < m && grid[cr][cc + 1] == 1) {
-                    grid[cr][cc + 1] = 2;
-                    que.add(new Pair(cr, cc + 1));
-                }
-                
-                // left
-                if(cc - 1 >= 0 && grid[cr][cc - 1] == 1) {
-                    grid[cr][cc - 1] = 2;
-                    que.add(new Pair(cr, cc - 1));
-                }
-                
-                // top
-                if(cr - 1 >= 0 && grid[cr - 1][cc] == 1) {
+                // do we have a fresh orange above
+                if (cr - 1 >= 0 && grid[cr - 1][cc] == 1) {
+                    //  rotten that orange                   
                     grid[cr - 1][cc] = 2;
-                    que.add(new Pair(cr - 1, cc));
+                    //  add to que
+                    que.add (new Pair (cr - 1, cc));
                 }
                 
-                // bottom
-                if(cr + 1 < n && grid[cr + 1][cc] == 1) {
+                // do we have a fresh orange below
+                if (cr + 1 < n && grid[cr + 1][cc] == 1) {
+                    //  rotten that orange                   
                     grid[cr + 1][cc] = 2;
-                    que.add(new Pair(cr + 1, cc));
+                    //  add to que
+                    que.add (new Pair (cr + 1, cc));
                 }
+                
+                // do we have a fresh orange left
+                if (cc - 1 >= 0 && grid[cr][cc - 1] == 1) {
+                    //  rotten that orange                   
+                    grid[cr][cc - 1] = 2;
+                    //  add to que
+                    que.add (new Pair (cr, cc - 1));
+                }
+                
+                // do we have a fresh orange right
+                if (cc + 1 < m && grid[cr][cc + 1] == 1) {
+                    //  rotten that orange                   
+                    grid[cr][cc + 1] = 2;
+                    //  add to que
+                    que.add (new Pair (cr, cc + 1));
+                }
+                
+                size--;
             }
-            time++;
+            level++;
         }
         
-        for(int r = 0; r < n; r++) {
-            for(int c = 0; c < m; c++) {
-                if(grid[r][c] == 1) {
+        // check does any cell has a fresh, hence return -1
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    // a fresh orange is found
                     return -1;
                 }
             }
         }
         
-        return time == -1 ? 0 : time;
+        if (level == 0) {
+            return 0;
+        }
+        
+        //  as first level is already rotten hence minTimeTaken = level - 1      
+        return level - 1;
     }
 }
