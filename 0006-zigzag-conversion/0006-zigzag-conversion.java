e4481193c4a80299b0137f876b1a702f6eec8d5f
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2023-02-04 10:20:04 +0530

    Time: 3 ms (95.40%), Space: 42.2 MB (97.96%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        
+        int tcnt = s.length();
+        int cnt = 0;
+        
+        StringBuilder sb = new StringBuilder("");
+        int idx = 0;
+        
+        int jumps = (numRows - 1) * 2;
+            
+        for (int i = idx; i < s.length(); i += jumps) {
+            sb.append(s.charAt(i));
+        }
+        idx++;
+        
+        int currJump = jumps;
+        while (idx + 1 < numRows) {
+            currJump -= 2;
+            
+            boolean comp = false;
+            
+            for (int i = idx; i < s.length(); ) {
+                sb.append(s.charAt(i));
+                if (comp == false) {
+                    comp = true;
+                    i += currJump;
+                } else {
+                    comp = false;
+                    i += (jumps - currJump);
+                }
+            }
+            
+            idx++;
+        }
+        
+        for (int i = idx; i < s.length(); i += jumps) {
+            sb.append(s.charAt(i));
+        }
+        idx++;
+        
+        return sb.toString();
+    }
+}
