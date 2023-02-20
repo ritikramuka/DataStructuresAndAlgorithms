7b6a1c6b4ac29d239c08ec578116fb038ca3731d
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2023-02-20 22:31:35 +0530

    Time: 1 ms (18.90%), Space: 40.7 MB (22.73%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,24 +14,54 @@
  * }
  */
 class Solution {
-    public void poTraversal (TreeNode root,  List<Integer> ans) {
-        if (root == null) return;
+    class Pair {
+        TreeNode node;
+        // call = 1 -> call left side, call = 2 -> call right side, call = 3 -> remove me         
+        int call;
         
-//         add my self
-        ans.add(root.val);
-        
-//         call for left
-        poTraversal (root.left, ans);
-        
-//         call for right
-        poTraversal (root.right, ans);
+        Pair (TreeNode node) {
+            this.node = node;
+            // always call left side first          
+            this.call = 1;
+        }
     }
     
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
+        List<Integer> pre = new ArrayList<>();
         
-        poTraversal (root, ans);
+        if (root == null) {
+            return pre;
+        }
         
-        return ans;
+        Stack<Pair> callStack = new Stack<>();
+        callStack.push(new Pair(root));
+        
+        while (callStack.size() != 0) {
+            Pair rpair = callStack.peek();
+            
+            if (rpair.call == 1) {
+                pre.add(rpair.node.val);
+                
+                // call my left side   
+                if (rpair.node.left != null) {
+                    TreeNode leftNode = rpair.node.left;
+                    callStack.push(new Pair(leftNode));
+                }
+                
+                rpair.call = 2;
+            } else if (rpair.call == 2) {
+                // call my right side   
+                if (rpair.node.right != null) {
+                    TreeNode rightNode = rpair.node.right;
+                    callStack.push(new Pair(rightNode));
+                }
+                
+                rpair.call = 3;
+            } else {
+                callStack.pop();
+            }
+        }
+        
+        return pre;
     }
 }
