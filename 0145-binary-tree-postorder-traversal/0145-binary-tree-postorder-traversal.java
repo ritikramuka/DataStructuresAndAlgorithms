119b46392e6bdd3bc7e5e0feb28cc69ff7231943
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2023-02-20 19:43:10 +0530

    Time: 1 ms (28.36%), Space: 40.4 MB (76.21%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -14,25 +14,43 @@
  * }
  */
 class Solution {
-    public void postTraversal (TreeNode root, List<Integer> ans) {
-        if (root == null) {
-            return;
+    class Node {
+        TreeNode node;
+        int calls;
+        
+        Node(TreeNode node) {
+            this.node = node;
+            this.calls = 0;
         }
-        
-//         call left subtree
-        postTraversal(root.left, ans);
-        
-//         call right subtree
-        postTraversal(root.right, ans);
-        
-//         add myself
-        ans.add(root.val);
-    } 
+    }
     
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        postTraversal (root, ans);
+        List<Integer> post = new ArrayList<>();
         
-        return ans;
+        if (root == null) {
+            return post;
+        }
+        
+        Stack<Node> st = new Stack<>();
+        st.push(new Node(root));
+        
+        while (st.size() != 0) {
+            Node top = st.peek();
+            
+            if (top.calls < 1 && top.node.left != null) {
+                Node left = new Node(top.node.left);
+                top.calls = 1;
+                st.push(left);
+            } else if (top.calls < 2 && top.node.right != null) {
+                Node right = new Node(top.node.right);
+                top.calls = 2;
+                st.push(right);
+            } else {
+                post.add(top.node.val);
+                st.pop();
+            }
+        }
+        
+        return post;
     }
 }
