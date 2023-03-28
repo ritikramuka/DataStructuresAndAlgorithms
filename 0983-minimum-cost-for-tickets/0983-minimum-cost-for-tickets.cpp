9fc98b8f44dfda1a420fb565244dede7de5a8ae0
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2023-03-28 09:18:24 +0530

    Time: 13 ms (8.58%), Space: 10.2 MB (11.90%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int memo(int day, unordered_set<int>& set, vector<int>& costs, vector<int>& dp) {
+        if(day >= 366) {
+            return 0;
+        }
+        
+        if(dp[day] != 0) {
+            return dp[day];
+        } 
+        
+        if(set.find(day) != set.end()) {
+            return dp[day] = min({memo(day + 1, set, costs, dp) + costs[0], memo(day + 7, set, costs, dp) + costs[1], memo(day + 30, set, costs, dp) + costs[2]});
+        } else {
+            return dp[day] = memo(day + 1, set, costs, dp);
+        }
+    }
+    
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        unordered_set<int> set (days.begin(), days.end());
+        vector<int> dp(366, 0);
+        return memo(1, set, costs, dp);
+    }
+};
