b128b8ac405e1574fc2a5be8b312767f578daf72
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2023-03-29 07:40:03 +0530

    Time: 9 ms (30.15%), Space: 43.1 MB (12.63%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -1,29 +1,17 @@
 class Solution {
-    public int memo(int[] satisfaction, int idx, int time, int[][] dp) {
-        if (idx ==  satisfaction.length) {
-            return 0;
-        } 
-        
-        if (dp[idx][time] != -1) {
-            return dp[idx][time];
-        }
-        
-        int inc = memo(satisfaction, idx + 1, time + 1, dp) + satisfaction[idx] * time;
-        int exc = memo(satisfaction, idx + 1, time, dp);
-        
-        return dp[idx][time] = Math.max(inc, exc);
-    }
-    
     public int maxSatisfaction(int[] satisfaction) {
         int n = satisfaction.length;
         
         Arrays.sort(satisfaction);
         
-        int[][] dp = new int[n][n + 1];
-        for (int[] a : dp) {
-            Arrays.fill(a, -1);
+        int[][] dp = new int[n + 1][n + 2];
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n; j >= 1; j--) {
+                dp[i][j] = Math.max(dp[i + 1][j + 1] + satisfaction[i] * j, dp[i + 1][j]);
+            }
         }
         
-        return memo(satisfaction, 0, 1, dp);
+        return dp[0][1];
     }
 }
