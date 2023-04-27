074020af52825cd59bc087afbc078a8148e08975
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2023-04-27 20:36:48 +0530

    Time: 1 ms (86.99%), Space: 43.3 MB (6.84%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -17,35 +17,30 @@ class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> lo = new ArrayList<>();
         
-//         if no root, hence return empty List
         if (root == null) {
             return lo;
         }
         
         Queue<TreeNode> que = new ArrayDeque<>();
-//         step 1: add root
         que.add(root);
         
-//         step 2: do your work till que size is greater than zero
-        while (que.size() > 0) {
-//             step 3: get curr size of the que
+        while (que.size() != 0) {
             int size = que.size();
+            
             List<Integer> currLevel = new ArrayList<>();
             
-//             step 4: remove size amt of fronts from the que and add there chilren, these size amt of people will lie on same level
-            while (size > 0) {
+            while (size-->0) {
                 TreeNode rnode = que.remove();
+                
                 currLevel.add(rnode.val);
-
-//              step 5: see if children are there, add them   
+                
                 if (rnode.left != null) {
                     que.add(rnode.left);
                 }
+                
                 if (rnode.right != null) {
                     que.add(rnode.right);
                 }
-                
-                size--;
             }
             
             lo.add(currLevel);
