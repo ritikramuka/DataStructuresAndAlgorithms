29069f67f9f354de2883cda7191b9fd8988678be
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2023-04-29 10:29:05 +0530

    Added solution - LeetHub

--- Burning Tree - GFG/burning-tree.java
+++ Burning Tree - GFG/burning-tree.java
@@ -127,91 +127,77 @@ class Solution
     	}
     }*/
     
-    static void setParent(Node root, HashMap<Node, Node> parent) 
-    {
-        if (root == null) 
-        {
+    static void setParent(Node root, HashMap<Node, Node> parent) {
+        if (root == null) {
             return;
         }
         
-        if (root.left != null) 
-        {
-            parent.put(root.left, root);    
+        if (root.left != null) {
+            parent.put(root.left, root);
         }
         
-        if (root.right != null) 
-        {
-            parent.put(root.right, root);    
+        if (root.right != null) {
+            parent.put(root.right, root);
         }
         
         setParent(root.left, parent);
         setParent(root.right, parent);
     }
     
-    static Node find(Node root, int target) 
-    {
-        if (root == null) 
-        {
+    static Node find(Node root, int target) {
+        if (root == null) {
             return null;
         }
         
-        if (root.data == target) 
-        {
+        if (root.data == target) {
             return root;
         }
         
         Node filc = find(root.left, target);
-        if (filc != null) 
-        {
+        if (filc != null) {
             return filc;
         }
         
         Node firc = find(root.right, target);
-        if (firc != null) 
-        {
+        if (firc != null) {
             return firc;
         }
         
         return null;
     }
     
-    // TC: O(N), SC: O(N)
     public static int minTime(Node root, int target) 
     {
         // Your code goes here
-        HashMap<Node, Node> parent = new HashMap();
+        HashMap<Node, Node> parent = new HashMap<>();
         setParent(root, parent);
         
         Node src = find(root, target);
         
+        // BFS
+        HashSet<Node> vis = new HashSet<>();
+        
         Queue<Node> que = new ArrayDeque<>();
         que.add(src);
-        
-        HashSet<Node> vis = new HashSet<>();
         vis.add(src);
         
         int level = 0;
-        while (que.size() != 0) 
-        {
+        while (que.size() != 0) {
             int size = que.size();
-            while (size-->0) 
-            {
+            while (size-->0) {
                 Node rnode = que.remove();
                 
-                if (rnode.left != null && vis.contains(rnode.left) == false) 
-                {
+                if (rnode.left != null && vis.contains(rnode.left) == false) {
                     que.add(rnode.left);
                     vis.add(rnode.left);
                 }
                 
-                if (rnode.right != null && vis.contains(rnode.right) == false) 
-                {
+                if (rnode.right != null && vis.contains(rnode.right) == false) {
                     que.add(rnode.right);
                     vis.add(rnode.right);
                 }
                 
-                if (parent.containsKey(rnode) == true && vis.contains(parent.get(rnode)) == false) 
-                {
+                if (parent.get(rnode) != null && vis.contains(parent.get(rnode)) == false) {
                     que.add(parent.get(rnode));
                     vis.add(parent.get(rnode));
                 }
@@ -219,6 +205,12 @@ class Solution
             level++;
         }
         
-        return level - 1;
+        int time = level - 1;
+        
+        if (time < 0) {
+            return 0;
+        }
+        
+        return time;
     }
 }
