2f10c846652969445f62a83003b29982c52a3157
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2023-05-02 11:59:40 +0530

    Time: 1 ms (84.61%), Space: 40.2 MB (38.99%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[][] createPascalsTriangle(int n) {
-        int[][] pascalsTrianlge = new int[n][n];
+        int[][] pascalsTrianlge = new int[n][3];
         
         for (int i = 0; i < n; i++) {
             pascalsTrianlge[i][0] = 1;
         }
         
         for (int i = 1; i < n; i++) {
-            for (int j = 1; j <= i; j++) {
+            for (int j = 1; j <= 2; j++) {
                 pascalsTrianlge[i][j] = pascalsTrianlge[i - 1][j] + pascalsTrianlge[i - 1][j - 1];
             }
         }
         
         return pascalsTrianlge;
     }
+    
     
     public int numIdenticalPairs(int[] nums) {
         int[] freq = new int[101];
