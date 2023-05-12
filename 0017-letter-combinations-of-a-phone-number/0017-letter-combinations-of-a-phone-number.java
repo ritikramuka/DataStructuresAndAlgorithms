44941f25478d70b981df2fc40c8f59d5d911ab95
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2023-05-13 04:09:57 +0530

    Time: 5 ms (43.38%), Space: 42.9 MB (17.90%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,25 +1,28 @@
 class Solution {
-    
-    void getCombinations(String digits, int pos, String asf, String[] letter, List<String> ans) {
+    void getAllCombinations(String digits, String asf, int pos, String[] letters, ArrayList<String> ans) {
         if (pos == digits.length()) {
             if (asf.length() > 0) {
                 ans.add(asf);
             }
+            
             return;
         } 
         
+        
         char digit = digits.charAt(pos);
-        for (int i = 0; i < letter[digit - '0'].length(); i++) {
-            getCombinations(digits, pos + 1, asf + letter[digit - '0'].charAt(i), letter, ans);
+        
+        String letter = letters[digit - '0'];
+        
+        for (int i = 0; i < letter.length(); i++) {
+            char ch = letter.charAt(i);
+            getAllCombinations(digits, asf + ch, pos + 1, letters, ans);
         }
     }
     
     public List<String> letterCombinations(String digits) {
-        String[] letter = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        List<String> ans = new ArrayList<>();
-        
-        getCombinations(digits, 0, "",letter, ans);
-        
+        String[] letters = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        ArrayList<String> ans = new ArrayList<>();
+        getAllCombinations(digits, "", 0, letters, ans);
         return ans;
     }
 }
