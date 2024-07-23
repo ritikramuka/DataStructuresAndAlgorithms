69bbe9b297b75df5742375124c25de26fb43a449
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2024-07-24 04:51:56 +0530

    Time: 13 ms (83.45%), Space: 45.4 MB (77.86%) - LeetHub

---
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.java
@@ -0,0 +1,38 @@
+class DiningPhilosophers {
+    ReentrantLock lock = new ReentrantLock();
+    Condition thinking = lock.newCondition();
+    AtomicBoolean isLeftAvailable = new AtomicBoolean(true);
+    AtomicBoolean isRightAvailable = new AtomicBoolean(true);
+
+    public DiningPhilosophers() {
+        
+    }
+
+    // call the run() method of any runnable to execute its code
+    public void wantsToEat(int philosopher, Runnable pickLeftFork, Runnable pickRightFork, Runnable eat, Runnable putLeftFork, Runnable putRightFork) throws InterruptedException {
+        lock.lock();
+        try {
+            while (!isLeftAvailable.get() || !isRightAvailable.get()) {
+                thinking.await();
+            }
+            
+            pickLeftFork.run();
+            isLeftAvailable.set(false);
+
+            pickRightFork.run();
+            isRightAvailable.set(false);
+            
+            eat.run();
+            
+            putLeftFork.run();
+            isLeftAvailable.set(true);
+            thinking.signal();
+            
+            putRightFork.run();
+            isRightAvailable.set(true);
+            thinking.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
