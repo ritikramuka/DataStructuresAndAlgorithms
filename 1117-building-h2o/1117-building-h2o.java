a663ef6373161c3270b1e704d78abc2bf5abe527
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2024-07-26 01:20:34 +0530

    Time: 19 ms (65.00%), Space: 45.1 MB (50.14%) - LeetHub

--- 1117-building-h2o/1117-building-h2o.java
+++ 1117-building-h2o/1117-building-h2o.java
@@ -1,51 +1,53 @@
 class H2O {
     
     ReentrantLock lock = new ReentrantLock();
-    Condition relO = lock.newCondition();
-    Condition relH = lock.newCondition();
-    
-    AtomicInteger cntOfHReleased = new AtomicInteger(0);
-    AtomicBoolean startORelease = new AtomicBoolean(true);
+    Boolean isOxygenBlocked = false;
+    int numOfHydrogenReleased = 0;
+    Condition canReleaseHydrogen = lock.newCondition();
+    Condition canReleaseOxygen = lock.newCondition();
 
     public H2O() {
         
     }
 
     public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
-        lock.lock();
-        
+		lock.lock();
         try {
-            while (startORelease.get()) {
-                relH.await();
+            while (!isOxygenBlocked || numOfHydrogenReleased >= 2) {
+                canReleaseHydrogen.await();
             }
 
             releaseHydrogen.run();
-            cntOfHReleased.getAndIncrement();
-            if (cntOfHReleased.get() == 2) {
-                cntOfHReleased.set(0);
-                startORelease.set(true);
-                relO.signal();
+            numOfHydrogenReleased++;
+
+            if (numOfHydrogenReleased == 2) {
+                isOxygenBlocked = false;
+                canReleaseOxygen.signal();
             }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         } finally {
             lock.unlock();
         }
-        
     }
 
     public void oxygen(Runnable releaseOxygen) throws InterruptedException {
         lock.lock();
-        
         try {
-            while (!startORelease.get()) {
-                relO.await();
+            while(isOxygenBlocked) {
+                canReleaseOxygen.await();
             }
 
             releaseOxygen.run();
-            startORelease.set(false);
-            relH.signalAll();
+            numOfHydrogenReleased = 0;
+
+            isOxygenBlocked = true;
+
+            canReleaseHydrogen.signalAll();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         } finally {
             lock.unlock();
         }
-        
     }
 }
