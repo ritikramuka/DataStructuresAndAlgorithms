7511010ef6a8b503807cfa6527fc2d53c860bfa9
Ritik Ramuka <56073559+ritikramuka@users.noreply.github.com>
2024-07-31 04:12:07 +0530

    Time: 8 ms (87.74%), Space: 49.7 MB (83.85%) - LeetHub

--- 1242-web-crawler-multithreaded/1242-web-crawler-multithreaded.java
+++ 1242-web-crawler-multithreaded/1242-web-crawler-multithreaded.java
@@ -8,12 +8,17 @@
 
 class Solution {
     private int maxWorker = 5;
+    private Queue<String> urlToParseQ = new LinkedList<>();
+    private HashSet<String> parsed = new HashSet<>();
+    private String hostName = "";
+    private ReentrantLock qLock = new ReentrantLock();
+    private Condition qCdn = qLock.newCondition();
+    private HashSet<Integer> workerIdle = new HashSet<>();
     
     public String getHostname(String url) {
         int startIdx = url.indexOf("//") + 2;
         int endIdx = url.indexOf('/', startIdx);
 
-        // Remember case when endIdx return -1, end of string
         if (endIdx == -1) {
             return url.substring(startIdx);
         }
@@ -21,84 +26,79 @@ class Solution {
         return url.substring(startIdx, endIdx);
     }
     
+    public void work(int workerId, HtmlParser htmlParser) {
+        while (true) {
+            String currUrl = null;
+            
+            qLock.lock();
+            try {
+                while (urlToParseQ.isEmpty()) {
+                    workerIdle.add(workerId);
+                    
+                    // if all are idle return
+                    if (workerIdle.size() == maxWorker) {
+                        qCdn.signalAll();
+                        return;
+                    }
+                    
+                    qCdn.await();
+                    workerIdle.remove(workerId);
+                }
+                
+                currUrl = urlToParseQ.poll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                qLock.unlock();
+            }
+            
+            if (currUrl == null) {
+                continue;
+            }
+            
+            List<String> nextUrls = htmlParser.getUrls(currUrl);
+            
+            qLock.lock();
+            try {
+                for (String nextUrl: nextUrls) {
+                    if (parsed.contains(nextUrl) == false && hostName.equals(getHostname(nextUrl))) {
+                        urlToParseQ.add(nextUrl);
+                        parsed.add(nextUrl);
+                        
+                        qCdn.signal();
+                    } 
+                }
+            } catch (Error e) {
+                e.printStackTrace();
+            } finally {
+                qLock.unlock();
+            }
+        }
+    }
+    
     public List<String> crawl(String startUrl, HtmlParser htmlParser) {
-        String hostName = getHostname(startUrl);
-        HashSet<String> parsed = new HashSet<>();
-        Queue<String> urlQ = new LinkedList<>();
+        hostName = getHostname(startUrl);
         
-        urlQ.add(startUrl);
+        urlToParseQ.add(startUrl);
         parsed.add(startUrl);
         
-        ReentrantLock urlQLock = new ReentrantLock();
-        Condition urlQCdn = urlQLock.newCondition();
-        HashSet<Integer> idleWorker = new HashSet<>();
-        
+        // pool of threads: workers to do work concurrently
         Thread[] workers = new Thread[maxWorker];
         for (int i = 0; i < maxWorker; i++) {
             int workerId = i;
-            Thread thread = new Thread(() -> {
-                while (true) {
-                    String currUrl = null;
-
-                    try {
-                        urlQLock.lock();
-                        while (urlQ.isEmpty()) {
-                            idleWorker.add(workerId);
-                            if (idleWorker.size() == maxWorker) {
-                                urlQCdn.signalAll();
-                                return;
-                            }
-
-                            try {
-                                urlQCdn.await(); 
-                            } catch (InterruptedException e) {
-                                return;
-                            }
-
-                            idleWorker.remove(workerId);
-                        }
-
-                        currUrl = urlQ.poll();
-                        if (currUrl == null) {
-                            continue;
-                        }
-                    } finally {
-                        urlQLock.unlock();
-                    }
-
-                    // get next urls
-                    List<String> nextUrls = htmlParser.getUrls(currUrl);
-
-                    urlQLock.lock();
-                    for (String nurl: nextUrls) {
-                        String localHostName = getHostname(nurl);
-                        if (parsed.contains(nurl) == false && localHostName.equals(hostName)) {
-                            urlQ.add(nurl);
-                            parsed.add(nurl);
-                            urlQCdn.signal();
-                        }
-                    }
-                    
-                    if (urlQ.isEmpty()) {
-                        idleWorker.add(workerId);
-                        urlQCdn.signalAll();
-                    }
-                    urlQLock.unlock();                 
-                }
-            });
-            workers[i] = thread;
-            workers[i].start();
+            Thread worker = new Thread(() -> work(workerId, htmlParser));
+            workers[workerId] = worker;
+            workers[workerId].start();
         }
         
         for (int i = 0; i < maxWorker; i++) {
             try {
-                workers[i].join();  
-            } catch(InterruptedException e) {
-                System.out.println("Error occurred");
-                return new ArrayList();
+                workers[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
         
-        return new ArrayList(parsed);
+        return new ArrayList<>(parsed);
     }
 }
